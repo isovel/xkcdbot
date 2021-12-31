003b549ce0d3da64babeb3661dc1b9a4cc3a4282
toastythetoaster <toast@isota.ch>
2021-12-31 06:42:42 -0500

    it actually works now :^)

--- commands/comic.js
+++ commands/comic.js
@@ -4,10 +4,10 @@ module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('comic')
 		.setDescription('placeholder')
-        .addIntegerOption(option => { return option.setName('number').setDescription('placeholder').setRequired(true); }),
+        .addIntegerOption(option => option.setName('number').setDescription('placeholder').setRequired(true)),
 		async execute(interaction) {
 			await interaction.reply({
-				content: `https://xkcd.com/${interaction.getIntegerOption('number')}/`
+				content: `https://xkcd.com/${interaction.options.getInteger('number')}/`
 			});
 		}
 };
